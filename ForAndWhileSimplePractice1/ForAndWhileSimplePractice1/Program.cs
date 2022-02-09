bfaef9feb2b573a9126d1d59450a2ef784d4a9db
Sarvin Nami <srvn0nm@gmail.com>
2022-02-09 22:49:46 +0330

    print all numbers up to a given one

---
+++ ForAndWhileSimplePractice1/ForAndWhileSimplePractice1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForAndWhileSimplePractice1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your number: ");
+            int range = int.Parse(Console.ReadLine());
+            //for (int i = 0; i < range; i++)
+            //{
+            //    Console.WriteLine(i+1);
+            //}
+            int i = 0;
+            while(i < range)
+            {
+                Console.WriteLine(i+1);
+                i++;
+            }
+        }
+    }
+}
