13dcf516fde8e78e6b639de47f0243382cc8107a
Sarvin Nami <srvn0nm@gmail.com>
2022-02-09 22:06:02 +0330

    new project to show power of a given number

---
+++ Power/Power/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Power
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to see It's square: ");
+            Console.WriteLine(Math.Pow((double.Parse(Console.ReadLine())),2));
+        }
+    }
+}
