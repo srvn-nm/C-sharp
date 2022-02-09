64ce325f787b537ffa7fc880ff9bdb618da2cf7f
Sarvin Nami <srvn0nm@gmail.com>
2022-02-09 22:59:47 +0330

    print sum and multiplication of all numbers up to a given one

--- ForAndLoopSimplePractice2/ForAndLoopSimplePractice2/Program.cs
+++ ForAndLoopSimplePractice2/ForAndLoopSimplePractice2/Program.cs
@@ -10,6 +10,24 @@ namespace ForAndLoopSimplePractice2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter your number here : ");
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int multi = 1;
+            //for (int i = 1; i <= number; i++)
+            //{
+            //    sum += i;
+            //    multi *= i;
+            //}
+            int i = 1;
+            while (i <= number)
+            {
+                sum += i;
+                multi *= i;
+                i++;
+            }
+            Console.WriteLine("Sum : " + sum);
+            Console.WriteLine("Multiplication : " + multi);
         }
     }
 }
