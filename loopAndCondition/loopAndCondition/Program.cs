412a9b3d568458095e822a031b420fd4abf332a9
Sarvin Nami <srvn0nm@gmail.com>
2022-02-10 13:35:48 +0330

    making my simple project better

--- loopAndCondition/loopAndCondition/Program.cs
+++ loopAndCondition/loopAndCondition/Program.cs
@@ -10,6 +10,32 @@ namespace loopAndCondition
     {
         static void Main(string[] args)
         {
+            int condition = 0;
+            while (condition < 10)
+            {
+                Console.WriteLine("Please enter what we need for calculating : ");
+                int num1 = int.Parse(Console.ReadLine());
+                String exe = Console.ReadLine();
+                int num2 = int.Parse(Console.ReadLine());
+                if (exe == "+")
+                {
+                    Console.WriteLine("ans : " + (num2 + num1));
+                }
+                else if (exe == "-")
+                {
+                    Console.WriteLine("ans : " + (num1 - num2));
+                }
+                else if (exe == "*")
+                {
+                    Console.WriteLine("ans : " + (num2 * num1));
+                }
+                else if (exe == "/")
+                {
+                    Console.WriteLine("ans : " + (num1 / num2));
+                }
+                condition++;
+            }
+            Console.WriteLine("Your turn is over!\nGoodBye ^-^");
         }
     }
 }
