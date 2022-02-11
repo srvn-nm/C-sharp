90bc37c3a5c4dda62013ff8fc8fac314ee6e8947
Sarvin Nami <srvn0nm@gmail.com>
2022-02-12 00:49:30 +0330

    function practice with calculating a circle variables

--- CircleAreaCalculatorFunction/CircleAreaCalculatorFunction/Program.cs
+++ CircleAreaCalculatorFunction/CircleAreaCalculatorFunction/Program.cs
@@ -8,8 +8,20 @@ namespace CircleAreaCalculatorFunction
 {
     internal class Program
     {
+        public static double Area(double radious)
+        {
+            return (Math.Pow(radious,2)*3.14);
+        }
+        public static double Perimeter(double radious)
+        {
+            return (2*radious*3.14);
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter the radious you want : ");
+            double radious = int.Parse(Console.ReadLine());
+            Console.WriteLine("------Area : "+ Area(radious));
+            Console.WriteLine("-Perimeter : "+Perimeter(radious));
         }
     }
 }
