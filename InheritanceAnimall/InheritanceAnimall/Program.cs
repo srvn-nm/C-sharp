9b6255cb3c93597b6482354ac239fd4a2d8a62e5
Sarvin Nami <srvn0nm@gmail.com>
2022-02-12 14:15:34 +0330

    testing all written features

--- InheritanceAnimall/InheritanceAnimall/Program.cs
+++ InheritanceAnimall/InheritanceAnimall/Program.cs
@@ -10,6 +10,21 @@ namespace InheritanceAnimall
     {
         static void Main(string[] args)
         {
+            Eagle eagle = new Eagle();
+            Tiger tiger = new Tiger();
+            eagle.name = "Chip";
+            eagle.weight = 20;
+            eagle.FlyHeight = 100;
+            eagle.age = 10;
+            eagle.Eat();
+            eagle.Sleep();
+            eagle.Fly();
+            tiger.name = "Rajah";
+            tiger.weight = 200;
+            tiger.age = 15;
+            tiger.Eat();
+            tiger.Sleep();
+            tiger.Hunt();
         }
     }
 }
