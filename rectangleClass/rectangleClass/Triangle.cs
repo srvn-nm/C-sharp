fedd0dbbe1698eee2ee2a7d7e32e5eacde38b3a9
Sarvin Nami <srvn0nm@gmail.com>
2022-02-12 12:48:50 +0330

    adding the triangle class with some practicing features

---
+++ rectangleClass/rectangleClass/Triangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rectangleClass
+{
+    internal class Triangle
+    {
+        public double Height { get; set; }
+        private double Base { get; set; }
+        public Triangle(double b)
+        {
+            Base = b;
+        }
+        public double Area()
+        {
+            return Height*0.5*Base;
+        }
+        public static void Name()
+        {
+            Console.WriteLine("Triangle");
+        }
+    }
+}
