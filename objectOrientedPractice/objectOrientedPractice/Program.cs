59cab3524084617b3e44760390c090258bcb74f6
Sarvin Nami <srvn0nm@gmail.com>
2022-02-12 11:41:40 +0330

    my object orriented project class person and random integer usage

--- objectOrientedPractice/objectOrientedPractice/Program.cs
+++ objectOrientedPractice/objectOrientedPractice/Program.cs
@@ -6,10 +6,31 @@ using System.Threading.Tasks;
 
 namespace objectOrientedPractice
 {
+    class Person
+    {
+        Random rnd = new Random();
+        private int Id;
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public Person()
+        {
+            Id = rnd.Next(1000);
+        }
+        public void Introduce()
+        {
+            Console.WriteLine("Hi!\nMy name is "+Name+" "+LastName+" and I am "+Age+" year's old.\nYou can find my identity with my random ID code : "+Id+".\nGoodbye ^-^");
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Person person = new Person();
+            person.Name = "Sarvin";
+            person.LastName = "Nami";
+            person.Age = 20;
+            person.Introduce();
         }
     }
 }
