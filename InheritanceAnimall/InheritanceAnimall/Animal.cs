ca87c1af72929b266ae59316c27cc18656495ac2
Sarvin Nami <srvn0nm@gmail.com>
2022-02-12 13:52:16 +0330

    adding animal and eagle class

--- InheritanceAnimall/InheritanceAnimall/Animal.cs
+++ InheritanceAnimall/InheritanceAnimall/Animal.cs
@@ -8,5 +8,16 @@ namespace InheritanceAnimall
 {
     internal class Animal
     {
+        public string name { get; set; }
+        public int age { get; set; }
+        public int weight { get; set; }
+        public void Eat()
+        {
+            Console.WriteLine(name+" is eating!");
+        }
+        public void Sleep()
+        {
+            Console.WriteLine(name+" is sleeping!");
+        }
     }
 }
--- InheritanceAnimall/InheritanceAnimall/Eagle.cs
+++ InheritanceAnimall/InheritanceAnimall/Eagle.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace InheritanceAnimall
 {
-    internal class Eagle
+    internal class Eagle: Animal
     {
+        public int FlyHeight { get; set; }
+        public void Fly()
+        {
+            Console.WriteLine(name + "is flying at" +FlyHeight);
+        }
     }
 }
