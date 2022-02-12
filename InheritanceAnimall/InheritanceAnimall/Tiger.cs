ad2b17409dba3fee77e0db7660a7c418912167df
Sarvin Nami <srvn0nm@gmail.com>
2022-02-12 14:09:52 +0330

    adding tiger class

---
+++ InheritanceAnimall/InheritanceAnimall/Tiger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceAnimall
+{
+    internal class Tiger:Animal
+    {
+        public int HuntSpeed { get; set; }
+        public void Hunt()
+        {
+            Console.WriteLine(name + " is hunting with " +HuntSpeed + " speed.");
+        }
+    }
+}
