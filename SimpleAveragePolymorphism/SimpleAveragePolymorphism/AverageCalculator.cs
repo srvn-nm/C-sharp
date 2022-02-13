4f6ccc7f5b68da8fb701d6b3a8706a3d136e5077
Sarvin Nami <srvn0nm@gmail.com>
2022-02-13 17:06:59 +0330

    starting polymorphism with simple average calculator

---
+++ SimpleAveragePolymorphism/SimpleAveragePolymorphism/AverageCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleAveragePolymorphism
+{
+    internal class AverageCalculator
+    {
+        public static double Ave(double a,double b)
+        {
+            return ((a+b)/2);
+        }
+        public static double Ave(double a, double b, double c)
+        {
+            return ((a + b+c) / 3);
+        }
+        public static double Ave(double a, double b, double c, double d)
+        {
+            return ((a + b + c+d) / 4);
+        }
+    }
+}
