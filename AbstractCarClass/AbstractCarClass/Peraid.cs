974dcaa951730aafd438e5c6080652155cd867a0
Sarvin Nami <srvn0nm@gmail.com>
2022-02-13 17:24:31 +0330

    adding peraid class

--- AbstractCarClass/AbstractCarClass/Peraid.cs
+++ AbstractCarClass/AbstractCarClass/Peraid.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace AbstractCarClass
 {
-    internal class Peraid
+    internal class Peraid : Car
     {
+        public override void move()
+        {
+            Console.WriteLine("Peraid is trying to move!");
+        }
     }
 }
