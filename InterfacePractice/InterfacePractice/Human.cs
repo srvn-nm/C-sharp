f318c033024f2ce68c55ecaf5df0e39da0358153
Sarvin Nami <srvn0nm@gmail.com>
2022-02-13 17:45:30 +0330

    adding human class implementing moving interface

---
+++ InterfacePractice/InterfacePractice/Human.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacePractice
+{
+    internal class Human : Moving
+    {
+        public void moving()
+        {
+            Console.WriteLine("Human is walking to the moon. ^-^");
+        }
+    }
+}
