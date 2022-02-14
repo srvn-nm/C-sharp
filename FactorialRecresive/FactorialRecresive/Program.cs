80f8635e036a458e56f6eb020838a30e3f9e3c32
Sarvin Nami <srvn0nm@gmail.com>
2022-02-15 01:03:38 +0330

    coding the first view of factorial

--- FactorialRecresive/FactorialRecresive/Program.cs
+++ FactorialRecresive/FactorialRecresive/Program.cs
@@ -8,8 +8,44 @@ namespace FactorialRecresive
 {
     internal class Program
     {
+        static long Factorial(long number)
+        {
+            if (number < 0)
+            {
+                return 0;
+            }
+            else if (number == 1 || number == 0)
+            {
+                return 1;
+            }
+            else { return number*Factorial(number-1); }
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter the number you want to see it's factorial here or e to exit : ");
+            string input = Console.ReadLine();
+            while (true)
+            {
+                if (input.All(char.IsDigit))
+                {
+                    long resault = Factorial(long.Parse(input));
+                    if(resault == 0)
+                    {
+                        Console.WriteLine("wrong input try again : ");
+                        input = Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine(resault+"\n***********************");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("wrong input try again : ");
+                    input = Console.ReadLine();
+                }
+            }
+
         }
     }
 }
