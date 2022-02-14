05fe140efb1a469840e348b755195b042c215700
Sarvin Nami <srvn0nm@gmail.com>
2022-02-14 10:41:45 +0330

    starting a new project

---
+++ DictionaryPractice/DictionaryPractice/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
