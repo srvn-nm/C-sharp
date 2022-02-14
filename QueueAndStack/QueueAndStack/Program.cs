a41164b1371928cfc0a6292e7496db4a349ab79d
Sarvin Nami <srvn0nm@gmail.com>
2022-02-14 13:43:25 +0330

    add Queue codes

--- QueueAndStack/QueueAndStack/Program.cs
+++ QueueAndStack/QueueAndStack/Program.cs
@@ -10,6 +10,27 @@ namespace QueueAndStack
     {
         static void Main(string[] args)
         {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+            Console.WriteLine(queue.Count);
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+            queue.Dequeue();
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+            queue.Dequeue();
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
