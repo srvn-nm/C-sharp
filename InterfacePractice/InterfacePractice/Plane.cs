d8e569932a431b78422f18aa190e773a8c10a0c0
Sarvin Nami <srvn0nm@gmail.com>
2022-02-14 09:02:09 +0330

    adding plane class

---
+++ InterfacePractice/InterfacePractice/Plane.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacePractice
+{
+    internal class Plane : Moving
+    {
+        public void moving()
+        {
+            Console.WriteLine("The plane is trying to reach the moon!");
+        }
+    }
+}
