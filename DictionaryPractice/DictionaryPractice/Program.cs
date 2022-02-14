a256f13be297a504161ac3a48613825011fe3b4f
Sarvin Nami <srvn0nm@gmail.com>
2022-02-14 10:51:40 +0330

    practicing dictionary

--- DictionaryPractice/DictionaryPractice/Program.cs
+++ DictionaryPractice/DictionaryPractice/Program.cs
@@ -10,6 +10,13 @@ namespace DictionaryPractice
     {
         static void Main(string[] args)
         {
+            Dictionary<string,int> dict = new Dictionary<string,int>();
+            dict["Sarvin"] = 6;
+            dict["Nami"] = 4;
+            dict["Abtin"] = 5;
+            dict["Zandi"] = 5;
+            Console.WriteLine("The value of key 'Sarvin' : "+dict["Sarvin"]);
+            Console.WriteLine("The value of key 'Abtin' : " + dict["Abtin"]);
         }
     }
 }
