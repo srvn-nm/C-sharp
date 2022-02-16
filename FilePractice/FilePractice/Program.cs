ca0a796a1106f2f06d26d528616f4797d35b9696
Sarvin Nami <srvn0nm@gmail.com>
2022-02-16 23:01:24 +0330

    using for loop to print the file

--- FilePractice/FilePractice/Program.cs
+++ FilePractice/FilePractice/Program.cs
@@ -13,14 +13,18 @@ namespace FilePractice
         {
             StreamReader sr = new StreamReader(@"../../../myFile.txt");
             //Console.WriteLine("Reading the whole file using this method : \n"+sr.ReadToEnd());
-            //Console.WriteLine("**************************");
+            //Console.WriteLine("**************************************");
             //Console.WriteLine("Reading one line of the file using this method : \n" + sr.ReadLine());
             List<String> list = new List<String>();
-            while (sr.EndOfStream)
+            while (!sr.EndOfStream)
             {
                 list.Add(sr.ReadLine());
             }
-
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine("The "+(i+1)+" line of the file : "+list[i]);
+                Console.WriteLine("**************************************");
+            }
         }
     }
 }
