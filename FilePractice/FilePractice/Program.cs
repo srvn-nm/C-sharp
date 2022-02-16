23d738d08119c4a75dfcf6cafb3a36d84261c78d
Sarvin Nami <srvn0nm@gmail.com>
2022-02-16 22:48:07 +0330

    reading the whole file

--- FilePractice/FilePractice/Program.cs
+++ FilePractice/FilePractice/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,8 @@ namespace FilePractice
     {
         static void Main(string[] args)
         {
+            StreamReader sr = new StreamReader(@"../../../myFile.txt");
+            Console.WriteLine(sr.ReadToEnd());
         }
     }
 }
